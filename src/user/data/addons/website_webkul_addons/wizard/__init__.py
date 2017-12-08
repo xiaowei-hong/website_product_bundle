9aa43e1842fbd7996dbd096eece7ed7ee9d18dfe
ziyou_liu_local <26520751@qq.com>
2017-12-08 12:32:20 -0800

    ziyou_liu_local

--- src/user/data/addons/website_webkul_addons/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#################################################################################
-#
-#   Copyright (c) 2016-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
-#	See LICENSE file for full copyright and licensing details.
-#################################################################################
-
-import wk_website_wizard
