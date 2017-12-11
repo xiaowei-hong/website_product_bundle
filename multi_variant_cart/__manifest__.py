3343547c0aecd524b5a80bf320b944502d0ceb8d
ziyou_liu_local <26520751@qq.com>
2017-12-11 09:27:29 -0800

    ziyou_liu_local

---
+++ multi_variant_cart/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Multi Variant Add to Cart',
+    'category': 'Website',
+    'summary': "List product attribute options on a product's details page with quantity Box with quick add to cart.",
+    'version': '10.0.1.0',
+    'author': 'Atharva System',
+    'description': """
+Multi Variant Add to Cart 
+===================
+List product attribute options on a product's details page with quantity Box with quick add to cart.
+        """,
+    'depends': ['website_sale'],
+    'installable': True,
+    'data': [  
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'images': ['static/description/icon.png'],
+    'support': 'support@atharvasystem.com',
+    'price': 45.00,
+    'currency': 'EUR',
+    'application': True,
+}
